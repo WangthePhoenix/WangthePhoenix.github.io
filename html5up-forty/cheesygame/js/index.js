e6733725ba616572e8d8286ffb19a2d7aa10b8b4
WangthePhoenix <wangp2@stconline.edu.hk>
2016-10-03 11:46:00 +0800

    uploaded site

---
+++ html5up-forty/cheesygame/js/index.js
@@ -0,0 +1,14 @@
+function poke1() {
+  
+  alert("You poked the Regular Cheese Omelette.")
+}
+
+function poke2() {
+  
+  alert("You poked the Cheese Omelette from Slightly Different Angle.");
+}
+
+function poke3() {
+  
+  alert("You poked the Cheese Omelette on Wide Display.");
+}
